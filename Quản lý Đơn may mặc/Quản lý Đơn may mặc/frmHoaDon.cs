f3cf895cdf432b7f1e451021c8dddd967a1752ca
Tú Trinh <ngothitutrinh1501@gmail.com>
2021-06-17 15:59:52 +0700

    Edit v2.1

--- Quản lý Đơn may mặc/Quản lý Đơn may mặc/frmHoaDon.cs
+++ Quản lý Đơn may mặc/Quản lý Đơn may mặc/frmHoaDon.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient; 
 
 namespace Quản_lý_Đơn_may_mặc
 {
     public partial class frmHoaDon : Form
     {
+        string sCon = "Data Source=DESKTOP-7QTS2T4;Initial Catalog=QUANLIMAYMAC;Integrated Security=True";
         public frmHoaDon()
         {
             InitializeComponent();
+        }
+
+        private void frmHoaDon_Load(object sender, EventArgs e)
+        {   //Bước 1
+            SqlConnection con = new SqlConnection(sCon);
+            try
+            {
+                con.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Xảy ra lỗi trong quá trình kết nối DB");
+            }
+            //Bước 2
+            string sQuery = "Select * from HOADON";
+            SqlDataAdapter adapter = new SqlDataAdapter(sQuery, con);
+
+            DataSet ds = new DataSet();
+
+            adapter.Fill(ds, "HOADON");
+
+            dgvHoaDon.DataSource = ds.Tables["HOADON"];
+
+            con.Close();//Bước 3
         }
 
     }
